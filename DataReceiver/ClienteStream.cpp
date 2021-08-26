d302fd75b68b887f8004ec92ff1b809e04dc6177
Gustavo Fresno <gustavo.fresno@codaly.st>
2021-08-26 16:02:42 -0300

    Primer Version DataReceiver - Solo es un cliente RSTP con la lib555

---
+++ DataReceiver/ClienteStream.cpp
@@ -0,0 +1,18 @@
+/*
+ * ClienteStream.cpp
+ *
+ *  Created on: Aug 26, 2021
+ *      Author: gustavo
+ */
+
+#include "ClienteStream.h"
+
+ClienteStream::ClienteStream() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ClienteStream::~ClienteStream() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ DataReceiver/ClienteStream.h
@@ -0,0 +1,17 @@
+/*
+ * ClienteStream.h
+ *
+ *  Created on: Aug 26, 2021
+ *      Author: gustavo
+ */
+
+#ifndef DATARECEIVER_CLIENTESTREAM_H_
+#define DATARECEIVER_CLIENTESTREAM_H_
+
+class ClienteStream {
+public:
+	ClienteStream();
+	virtual ~ClienteStream();
+};
+
+#endif /* DATARECEIVER_CLIENTESTREAM_H_ */
